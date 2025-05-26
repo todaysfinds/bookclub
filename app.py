8b42a9ae245c7f178472c6f10b46154b073aba0c
todaysfinds <todaysfindss@gmail.com>
2025-05-26 17:33:51 +0900

    password text

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ login_manager.login_view = 'login'  # 로그인 안 했을 때 이동할 기본 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(512), nullable=False)
+    password = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
